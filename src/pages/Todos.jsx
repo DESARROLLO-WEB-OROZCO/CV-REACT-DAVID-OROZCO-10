efbb97be5eb7caccff842fa2f262153f1bc8962c
david alejandro orozco henao <davidalejandroorozcohenao@gmail.com>
2025-12-05 21:57:15 -0500

    componente Todos.jsx con fetch inicial usando useEffect

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+
 export default function Todos() {
-  return <h1>Todos</h1>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(res => res.json())
+      .then(data => setTodos(data.slice(0, 10)));
+  }, []);
+
+  return (
+    <div>
+      <h1>Todos</h1>
+    </div>
+  );
 }
